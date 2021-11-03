93c5bf2799c3a81439ace0bf37e3c9c50f267cd1
hakacarol <haka.carolina@gmail.com>
2021-11-03 17:39:13 -0300

    subindo aulas

--- 06_integradora/06-integradora/src/components/ClassComponent.jsx
+++ 06_integradora/06-integradora/src/components/ClassComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class ClassComponent extends Component {
    render() {
        return (
-           <li>{this.props.nome} {this.props.estaNaLista ? "está" : "não está"} convidado para a festa</li>
+           <li>{this.props.nome} {this.props.estaNaLista ? "está" : "não está"} convidada para a festa</li>
        )
    }
 }
--- 06_integradora/06-integradora/src/container/App.js
+++ 06_integradora/06-integradora/src/container/App.js
@@ -1,23 +1,73 @@
 import ClassComponent from '../components/ClassComponent';
 import FunctionComponent from '../components/FunctionComponent';
 import '../styles/App.css';
- 
+import { Fragment } from 'react';
+
 function App() {
- return (
- <>
- <div className="App">
-  <h3>Convidado:</h3>
+
+  let convidados = [
+    {
+      id: 0,
+      nome: 'Carol',
+      estaNaLista: true,
+      tarefa: 'brigadeiro'
+    },
+    {
+      id: 1,
+      nome: 'Renata',
+      estaNaLista: true,
+      tarefa: 'bolinha de queijo'
+    },
+    {
+      id: 2,
+      nome: 'Clarissa',
+      estaNaLista: true,
+      tarefa: 'beijinho'
+    },
+    {
+      id: 3,
+      nome: 'Helena',
+      estaNaLista: false,
+      tarefa: 'cerveja'
+    }
+  ]
+  return (
+
+    <div className="App">
+      <h3>Convidadas:</h3>
+      {
+        convidados.map((convidado) => {
+          return (
+            <Fragment key={convidado.id}>
+              <ClassComponent nome={convidado.nome} estaNaLista={convidado.estaNaLista} />
+            </Fragment>
+          )
+        })
+      }
+
+      <h3>Tarefas: </h3>
+      {
+        convidados.map((convidado) => {
+          return (
+            <Fragment key={convidado.id}>
+              <FunctionComponent nome={convidado.nome} tarefa={convidado.tarefa}  />
+            </Fragment>
+          )
+        })
+
+      }
+
+      {/* 
   <ClassComponent nome="Nicolas" estaNaLista={true} />
   <ClassComponent nome="Pedro" estaNaLista={false} />
   <ClassComponent nome="Carolina" estaNaLista={true} />
   <h3>Tarefas: </h3>
   <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
   <FunctionComponent nome="Pedro" tarefa="pizza" />
-  <FunctionComponent nome="Carolina" tarefa="bebidas" />
-</div>
-</>
+  <FunctionComponent nome="Carolina" tarefa="bebidas" /> */}
+    </div>
 
  );
 }
- 
+
 export default App;
